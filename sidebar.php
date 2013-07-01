f1154d7541ff4d8e4355cf904544ca6c3ac7490e
Sheri Bigelow <sheri@designsimply.com>
2013-07-01 05:54:50 -0400

    Remove default widgets from sidebar.php.

--- sidebar.php
+++ sidebar.php
@@ -6,29 +6,8 @@
  * @since designsimply 1.0
  */
 ?>
-		<div id="secondary" class="widget-area" role="complementary">
-			<?php do_action( 'before_sidebar' ); ?>
-			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-				<aside id="search" class="widget widget_search">
-					<?php get_search_form(); ?>
-				</aside>
-
-				<aside id="archives" class="widget">
-					<h2 class="widget-title"><?php _e( 'Archives', 'designsimply' ); ?></h2>
-					<ul>
-						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-					</ul>
-				</aside>
-
-				<aside id="meta" class="widget">
-					<h2 class="widget-title"><?php _e( 'Meta', 'designsimply' ); ?></h2>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</aside>
-
-			<?php endif; // end sidebar widget area ?>
-		</div><!-- #secondary .widget-area -->
+	<div :wqclass="widget-area" role="complementary">
+		<?php do_action( 'before_sidebar' ); ?>
+		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+		<?php endif; // end sidebar widget area ?>
+	</div><!-- #secondary .widget-area -->
