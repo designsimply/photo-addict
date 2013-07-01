d33d508770b1cd6cc768fdd5fbcfce06601d2f5a
Sheri Bigelow <sheri@designsimply.com>
2013-07-01 05:50:42 -0400

    Add keyboard shortcuts "forward slash" to go to the parent page and "alt" for a random image—enqueued from functions.php.

--- js/keyboard-image-navigation.js
+++ js/keyboard-image-navigation.js
@@ -2,10 +2,16 @@ jQuery( document ).ready( function( $ ) {
 	$( document ).keydown( function( e ) {
 		var url = false;
 		if ( e.which == 37 ) {  // Left arrow key code
-			url = $( '.previous-image a' ).attr( 'href' );
+			url = $( '.previous a' ).attr( 'href' );
 		}
 		else if ( e.which == 39 ) {  // Right arrow key code
-			url = $( '.entry-attachment a' ).attr( 'href' );
+			url = $( '.next a' ).attr( 'href' );
+		}
+		else if ( e.which == 18 ) {  // alt key code
+			url = $( '.first-random-image' ).attr( 'href' );
+		}
+		else if ( e.which == 191 ) {  // Forward slash key code
+			url = $( '.post-parent' ).attr( 'href' );
 		}
 		if ( url && ( !$( 'textarea, input' ).is( ':focus' ) ) ) {
 			window.location = url;
