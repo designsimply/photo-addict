11bd03d8b79af9fb4e1aacc9b3da8f6c6fed79b2
Sheri Bigelow <sheri@designsimply.com>
2013-07-01 06:06:36 -0400

    Switch to WP_Query, add previous/next nag to the Writing section, remove image search fallback from the Speaking section (maybe add this back later), comment out bio and footer, and clean up spacing.

--- home.php
+++ home.php
@@ -13,140 +13,109 @@
  */
 
 get_header(); ?>
+	<?php //designsimply_tonesque_css(); ?>
 
-		<div id="main" role="main">
+	<section class="content" role="main">
+		<article>
+			<h2>Writing</h2>
+			<ul>
+			<?php
+				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+				$args = array(
+					'paged' => $paged,
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'post_format',
+							'field' => 'slug',
+							'terms' => array( 'post-format-status', 'post-format-image' , 'post-format-gallery' ),
+							'operator' => 'NOT IN'
+						)
+				) );
+				$query = new WP_Query( $args );
 
-			<?php //designsimply_tonesque_css(); // Print post-specific color css ?>
-			<section>
-				<h2>Writing</h2>
-				<ul>
-				<?php 
-					$args = array( 
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'post_format',
-								'field' => 'slug',
-								'terms' => array( 'post-format-status', 'post-format-image' , 'post-format-gallery' ),
-								'operator' => 'NOT IN'
-							)
-					) );
-					$query = new WP_Query( $args );
-
-					if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); 
-					$ids[] = get_the_ID(); ?>
-					<li><a href="<?php the_permalink() ?>"><?php echo $post->post_title; //echo substr( $post->post_title, 0, 37 ); ?></a></li>
-				<?php
-				endwhile; 
+				if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+				$ids[] = get_the_ID(); ?>
+				<li><a href="<?php the_permalink() ?>"><?php echo $post->post_title; //echo substr( $post->post_title, 0, 37 ); ?></a></li>
+			<?php
+				endwhile;
 				endif;
-				?>
-				</ul>
+			?>
+			</ul>
+			<?php designsimply_content_nav( 'nav-below' ); ?>
 
-				<?php designsimply_content_nav( 'nav-below' ); ?>
-			</section>
+		</article>
 
-			<section>
-				<h2>Speaking</h2>
+		<article>
+			<h2>Speaking</h2>
+			<?php
+				$args = array(
+					//'paged' => $paged,
+					'posts_per_page' => 1,
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'post_format',
+							'field' => 'slug',
+							'terms' => 'post-format-status',
+							'operator' => 'IN'
+						)
+				) );
+				$query = new WP_Query( $args );
+
+				if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+					<a href="<?php the_permalink() ?>" class="post-format-status"><?php the_post_format_image( 'medium' ); ?><br><?php the_title(); ?></a>
 				<?php
-					$args = array( 
-						'posts_per_page' => 1,
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'post_format',
-								'field' => 'slug',
-								'terms' => 'post-format-status',
-								'operator' => 'IN'
-							)
-					) );
-					$query = new WP_Query( $args );
+				endwhile;
+				endif;
+			?>
+			<?php //designsimply_content_nav( 'nav-below' ); ?>
+		</article>
 
-					if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
-						<a href="<?php the_permalink() ?>"><?php the_post_format_image( 'large' ); ?><br><?php the_title(); ?></a>
-					<?php 
-					endwhile; 
-					endif;
+		<article>
+			<h2>Photography</h2>
+			<div class="image-block">
+			<?php
+				$args = array(
+					//'paged' => $paged,
+					'posts_per_page' => 8,
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'post_format',
+							'field' => 'slug',
+							'terms' => array( 'post-format-gallery' ),
+							'operator' => 'IN'
+						)
+				) );
+				$query = new WP_Query( $args );
 
+				if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 
-					/*
-					$args = array( 'numberposts' => '1', 'tax_query' => array(
-							array(
-								'taxonomy' => 'post_format',
-								'field' => 'slug',
-								'terms' => 'post-format-status',
-								'operator' => 'IN'
-							)
-					) );
-					$recent_posts = wp_get_recent_posts( $args );
-					foreach($recent_posts as $recent ){
-						echo '<a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' . $recent["post_title"].'</a> ';
-						//the_post_format_image( 'medium' );
-						if ( has_post_thumbnail() )
-							the_post_thumbnail( 'medium' );
-						//echo '<script type="text/javascript" src="http://speakerdeck.com/embed/4f1a05d21a3f95001f0133df.js"></script>';
-						/* Medium random image
-						$all_images =& get_children( 'post_parent=&post_type=attachment&post_mime_type=image&numberposts=-1&poststatus=publish' );
-						$random = array_rand($all_images);
-						echo '<br><a href="' . get_permalink($random) . '#image">';
-						echo wp_get_attachment_image( $random, 'medium' );
-						echo '</a>';
-						
-					}
-					*/
-				?>
-			</section>
+					<a href="<?php the_permalink() ?>"><?php the_post_format_image( 'thumbnail' ); ?></a>
 
-			<section>
-				<h2>Photography</h2>
-				<ul>
-				<?php 
-					//query_posts(array('post__not_in' => $ids));
-					$args = array( 
-						'posts_per_page' => 6,
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'post_format',
-								'field' => 'slug',
-								'terms' => array( 'post-format-gallery', 'post-format-image' ),
-								'operator' => 'IN'
-							)
-					) );
-					$query = new WP_Query( $args );
+			<?php
+				endwhile;
+				endif;
+			?>
+			<?php //designsimply_content_nav( 'nav-below' ); ?>
+			</div>
+		</article>
+	</section><!-- .content -->
 
-					if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+		<?php //else : ?>
 
-						<li><a href="<?php the_permalink() ?>"><?php the_post_format_image( 'thumbnail' ); ?></a></li>
+			<?php //get_template_part( 'no-results', 'index' ); ?>
 
-					<?php 
-					endwhile;
-					endif;
+		<?php //endif; ?>
 
-					?>
-					</ul>
-					<?php
-						/*
-						$args = array( 'numberposts' => '6', 'tax_query' => array(
-								array(
-									'taxonomy' => 'post_format',
-									'field' => 'slug',
-									'terms' => 'post-format-image','post-format-gallery',
-									'operator' => 'IN'
-								)
-						) );
-						$latest_image_post = wp_get_recent_posts( $args );
-						foreach( $latest_image_posts as $recent ){
-							echo '<a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> ';
-							echo 'asdf'.get_the_post_format_image( 'thumbnail' );
-						}
-						*/
-					?>
-			</section>
+	<!--<section class="side" role="complimentary">
+		<aside class="bio">
+			<p><img class="alignleft gravatar" src="http://0.gravatar.com/avatar/198723e26f9350d9bbe8d4f35a8b0bb7?size=116" />
+				Sheri Bigelow is a Creativity Cultivator for Automattic. She has blended a love of photography, a degree in
+				Business & Chemistry, a geeky penchant for optimizing nginx, and adoration for WordPress to help the world
+				create beautiful websites they love at WordPress.com. She’s passionate about working with the extraordinary
+				collection of talent that is Automattic and can usually be found speaking at WordCamps, skiing the Rockies,
+				kayaking the Adirondacks, or exploring Saratoga Springs, NY.</p>
+		</aside>
+	</section> .side -->
 
-			<?php //else : ?>
-
-				<?php //get_template_part( 'no-results', 'index' ); ?>
-
-			<?php //endif; ?>
-
-		</div><!-- #main -->
-
-<?php //get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_sidebar(); ?>
+<?php //get_footer(); ?>
