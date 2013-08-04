ebdb23ac17ba74652401b4775bddd506b7d2315b
Sheri Bigelow <sheri@designsimply.com>
2013-08-04 10:50:26 -0400

    Rearrange content boxes and remove headings

--- home.php
+++ home.php
@@ -15,8 +15,35 @@
 get_header(); ?>
 
 	<section class="content" role="main">
+		<?php $args = array(
+		'posts_per_page' => 12,
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'post_format',
+				'field' => 'slug',
+				'terms' => array( 'post-format-gallery' ),
+				'operator' => 'IN'
+			)
+		) );
+		$query = new WP_Query( $args );
+
+		if ($query->have_posts()) : ?>
 		<article>
-			<h3>Writing</h3>
+			<div class="image-block">
+				<?php while ($query->have_posts()) : $query->the_post(); ?>
+					<?php if ( has_post_thumbnail() ) : ?>
+						<a href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'thumbnail' ); ?></a>
+					<?php elseif ( function_exists( 'the_post_format_image' ) ) : ?>
+						<a href="<?php the_permalink() ?>"><?php the_post_format_image( 'thumbnail' ); ?></a>
+					<?php else : ?>
+						<?php // @todo check to see if a gallery is present and get first gallery image, fallback to any image after that ?>
+					<?php endif;
+				endwhile; ?>
+			</div>
+		</article>
+		<?php endif; ?>
+
+		<article>
 			<ul>
 			<?php
 				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
@@ -33,21 +60,19 @@ get_header(); ?>
 				$query = new WP_Query( $args );
 
 				if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-				$ids[] = get_the_ID(); ?>
-				<li><a href="<?php the_permalink() ?>" <?php post_class(); ?>>
-				<?php // If the post doesn't have a title, print the month and day
-				if ( empty( $post->post_title ) ) :
-					echo the_date('F jS');
-				else : 
-					echo $post->post_title;
+					$ids[] = get_the_ID(); ?>
+					<li><a href="<?php the_permalink() ?>" <?php post_class(); ?>>
+					<?php // If the post doesn't have a title, print the month and day
+					if ( empty( $post->post_title ) ) :
+						echo the_date('F jS');
+					else : 
+						echo $post->post_title;
+					endif; ?>
+					</a></li>
+					<?php endwhile;
+					else :
+						get_template_part( 'no-results', 'index' );
 				endif; ?>
-				</a></li>
-			<?php
-				endwhile;
-				else :
-					get_template_part( 'no-results', 'index' );
-				endif;
-			?>
 			</ul>
 			<?php photo_addict_content_nav( 'nav-below' ); ?>
 
@@ -68,7 +93,6 @@ get_header(); ?>
 
 		if ($query->have_posts()) : ?>
 		<article>
-			<h3>Speaking</h3>
 			<?php while ($query->have_posts()) : $query->the_post();
 					// Look for an image associated with the post, fallback to the excerpt
 					$first_image = photo_addict_first_post_image_url( 'medium' );
@@ -80,37 +104,7 @@ get_header(); ?>
 					<?php endif;
 				endwhile; ?>
 		</article>
-		<?php endif; ?>
 		<?php //photo_addict_content_nav( 'nav-below' ); ?>
-
-		<?php $args = array(
-			//'paged' => $paged,
-			'posts_per_page' => 12,
-			'tax_query' => array(
-				array(
-					'taxonomy' => 'post_format',
-					'field' => 'slug',
-					'terms' => array( 'post-format-gallery' ),
-					'operator' => 'IN'
-				)
-		) );
-		$query = new WP_Query( $args );
-
-		if ($query->have_posts()) : ?>
-		<article>
-			<h3>Photography</h3>
-			<div class="image-block">
-				<?php while ($query->have_posts()) : $query->the_post(); ?>
-					<?php if ( has_post_thumbnail() ) : ?>
-						<a href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'thumbnail' ); ?></a>
-					<?php elseif ( function_exists( 'the_post_format_image' ) ) : ?>
-						<a href="<?php the_permalink() ?>"><?php the_post_format_image( 'thumbnail' ); ?></a>
-					<?php else : ?>
-						<?php // @todo check to see if a gallery is present and get first gallery image, fallback to any image after that ?>
-					<?php endif;
-				endwhile; ?>
-			</div>
-		</article>
 		<?php endif; ?>
 	</section><!-- .content -->
 
