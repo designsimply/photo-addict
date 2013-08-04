3c8d2f0f0627c6e6eb4eecc2ae4bf1e67f50ca39
Sheri Bigelow <sheri@designsimply.com>
2013-08-04 11:15:54 -0400

    Don't display the primary menu on the home page or attachment pages, display a footer sidebar on non-attachment pages, remove theme description footer, add SVG note

--- footer.php
+++ footer.php
@@ -9,21 +9,16 @@
  */
 ?>
 
-<?php if ( ( is_single() || is_page() ) && ! is_attachment() ) : ?>
+<?php if ( ! is_attachment() ) : ?>
 <nav role="navigation">
-	<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-	<?php dynamic_sidebar( 'sidebar-2' ); ?>
-</nav>
+	<?php if ( ! is_home() )
+		wp_nav_menu( array( 'theme_location' => 'primary' ) );
 
-<footer>
-	<?php /*printf( __( '%1$s is an experimental flexbox layout theme made by %2$s for %3$s with &hearts;', 'photo-addict' ),
-		'<a href="http://designsimply.com/theme/" rel="theme">Photo Addict</a>',
-		'<a href="http://designsimply.com/about/" rel="designer">Sheri Bigelow</a>',
-		'<a href="http://wordpress.org/" rel="generator">WordPress</a>'
-	);*/ ?>
-</footer>
+	dynamic_sidebar( 'sidebar-2' ); ?>
+</nav>
 <?php endif; ?>
 
+<!-- This SVG let's us add a blur effect in Firefox since CSS3 filter:blur() doesn't work there yet -->
 <svg height="0" xmlns="http://www.w3.org/2000/svg">
   <filter id="blur50" x="-5%" y="-5%" width="110%" height="110%">
     <feGaussianBlur in="SourceGraphic" stdDeviation="50"/>
