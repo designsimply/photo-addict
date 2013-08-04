0f5db4187c1d9635a44e3a491e639d187354c2eb
Sheri Bigelow <sheri@designsimply.com>
2013-08-04 11:49:18 -0400

    Untangle attachment-specific code since it was moved to image.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 /**
  * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package photo-addict
  * @since photo-addict 1.0
@@ -27,45 +22,20 @@ get_header(); ?>
 		</h2>
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<?php if ( is_attachment() ) : ?>
-				<nav>
-					<span class="previous"><?php previous_image_link( '%link', __( '<div class="genericon genericon-collapse"></div>', 'photo-addict' ) ); ?></span>
-					<span class="next"><?php next_image_link( '%link', __( '<div class="genericon genericon-expand"></div>', 'photo-addict' ) ); ?></span>
-				</nav>
-			<?php else : ?>
-				<nav>
-					<span class="previous"><?php next_post_link( '%link', __( '<div class="genericon genericon-collapse"></div>', 'photo-addict' ) ); ?></span>
-					<span class="next"><?php previous_post_link( '%link', __( '<div class="genericon genericon-expand"></div>', 'photo-addict' ) ); ?></span>
-				</nav>
-			<?php endif; ?>
+			<nav>
+				<span class="previous"><?php next_post_link( '%link', __( '<div class="genericon genericon-collapse"></div>', 'photo-addict' ) ); ?></span>
+				<span class="next"><?php previous_post_link( '%link', __( '<div class="genericon genericon-expand"></div>', 'photo-addict' ) ); ?></span>
+			</nav>
 
 			<?php if ( is_search() || is_archive() ) : // Only display excerpts for search and archives ?>
-				<div class="the_excerpt">
-					<?php the_excerpt(); ?>
-				</div><!-- .the_excerpt -->
-			<?php elseif ( is_attachment() ) : // Only display Excerpts for Search
-				$attachment_size = apply_filters( 'photo_addict_attachment_size', array( 640, 640 ) ); // Filterable image size.
-				//echo wp_get_attachment_image( $post->ID, $attachment_size );
-				echo wp_get_attachment_image( $post->ID, 'large' );
-				$image_attributes = wp_get_attachment_image_src( $post->ID, 'large' );
-				$image_width = max( $metadata['sizes']['large']['width'], $metadata['sizes']['medium']['width'], $metadata['sizes']['thumbnail']['width'] );
-				if ( ( ! $metadata['sizes']['large']['width'] > 0 || $metadata['width'] < $metadata['sizes']['large']['width'] ) && $metadata['width'] > $metadata['sizes']['medium']['width'] ) {
-					$image_width = $metadata['width'];
-				}
-				echo '<style>';
-				if ( $image_width > 0 ) {
-					echo '.attachment #wrapper { width: ' . $image_width . 'px; }';
-				}
-				echo '</style>';
-				?>
-				<div class="the-excerpt">
-					<?php the_excerpt(); ?>
-				</div><!-- .the_excerpt -->
+			<div class="the_excerpt">
+				<?php the_excerpt(); ?>
+			</div><!-- .the_excerpt -->
 			<?php else : ?>
-				<div class="the-content">
-					<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'photo-addict' ) ); ?>
-					<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'photo-addict' ), 'after' => '</div>' ) ); ?>
-				</div><!-- .entry-content -->
+			<div class="the-content">
+				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'photo-addict' ) ); ?>
+				<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'photo-addict' ), 'after' => '</div>' ) ); ?>
+			</div><!-- .entry-content -->
 			<?php endif; ?>
 
 			<?php if ( !is_page() ) : ?>
@@ -81,22 +51,11 @@ get_header(); ?>
 				<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="post-parent" rel="home"><?php bloginfo( 'name' ); ?></a>
 			</div>
 
-			<?php if ( 'attachment' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-				<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'photo-addict' ),'.' );
-				if ( $tags_list ) :
-				?>
-				<span class="tags-links">
-					<?php printf( __( 'Tagged %1$s', 'photo-addict' ), $tags_list ); ?>
-				</span>
-				<?php endif; // End if $tags_list ?>
-			<?php endif; // End if 'post' == get_post_type() ?>
-
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || '0' != get_comments_number() )
 					//comments_template( '', true );
+
 			?>
 
 		</article><!-- #post-<?php the_ID(); ?> -->
@@ -107,6 +66,6 @@ get_header(); ?>
 
 <?php endif; ?>
 
-<?php //if ( ! is_attachment() ) : get_sidebar(); endif; ?>
+<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
