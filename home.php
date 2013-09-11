aed6e1b4ee9c2c6c3c0c233d148e2ca423c1e521
Sheri Bigelow <designsimply@wildtoast.local>
2013-09-11 08:06:48 -0400

    Remove unneeded comments

--- home.php
+++ home.php
@@ -1,12 +1,6 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * Home page template file.
  *
  * @package photo-addict
  * @since photo-addict 1.0
@@ -102,7 +96,6 @@ get_header(); ?>
 					<?php endif;
 				endwhile; ?>
 		</article>
-		<?php //photo_addict_content_nav( 'nav-below' ); ?>
 		<?php endif; ?>
 	</section><!-- .content -->
 
