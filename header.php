792846b194453bf84a340a35508d2a33e70973b1
Sheri Bigelow <sheri@designsimply.com>
2013-09-12 17:52:29 -0400

    Remove extra </head>

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 <meta name="description" content="<?php echo $site_description; ?>" />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-</head>
 
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
