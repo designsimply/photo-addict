bdee75b1dae44df43f89696c804b0cc510de87d5
Sheri Bigelow <sheri@designsimply.com>
2023-12-07 15:33:44 -0800

    Add alt+r keyboard shortcut to roll the dice for a random image.

--- js/keyboard-image-navigation.js
+++ js/keyboard-image-navigation.js
@@ -1,10 +1,16 @@
 jQuery( document ).ready( function( $ ) {
 	$( document ).keydown( function( e ) {
 		var url = false;
-		if ( e.which == 37 ) {  // Left arrow key code
+		if ( e.which == 37 ) {  // 37 left arrow
 			url = $( '.previous a' ).attr( 'href' );
 		}
-		else if ( e.which == 39 ) {  // Right arrow key code
+		else if ( e.which == 39 ) {  // 39 right arrow
+			url = $( '.next a' ).attr( 'href' );
+		}
+		else if ( e.which == 75 ) {  // 75 k
+			url = $( '.previous a' ).attr( 'href' );
+		}
+		else if ( e.which == 74 ) {  // 74 j
 			url = $( '.next a' ).attr( 'href' );
 		}
 		else if ( e.which == 40 ) {  // Down arrow key code
@@ -13,18 +19,24 @@ jQuery( document ).ready( function( $ ) {
 					url = $( '.random-images a' ).attr( 'href' );
 					if (!url) { url = $( '.random-image' ).attr( 'href' ); }
 				}
+		else if ( e.which == 72 ) {  // 72 h
+				url = '/';
+		}
+		else if ( e.which == 82 ) {  // 82 r
+			if ( e.altKey ) { // alt modifier key
+					url = $( 'a#rollthedice' ).attr( 'href' );
 			}
 		}
-		else if ( e.which == 38 ) {  // Up arrow key code
-			if ( e.altKey ) { // Alt modifier key
-				if ( e.ctrlKey ) { // Control modifier key
+		else if ( e.which == 80 ) {  // 80 p
+			if ( e.altKey ) { // alt modifier key
+				if ( e.ctrlKey ) { // control modifier key
 					url = $( 'a.post-parent' ).attr( 'href' );
 				}
 			}
 		}
-		else if ( e.which == 76 ) {  // l key code
-			if ( e.shiftKey ) { // Shift modifier key
-				if ( e.ctrlKey ) { // Control modifier key
+		else if ( e.which == 76 ) {  // 76 l
+			if ( e.shiftKey ) { // shift modifier key
+				if ( e.ctrlKey ) { // control modifier key
 					window.location = keyboard_navigation_args.home_url + '/wp-login.php?redirect_to='+document.URL;
 				}
 			}
